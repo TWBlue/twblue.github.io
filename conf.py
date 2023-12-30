086423967c236a729b68e6cebec1430238d0b246
Manuel Cortez <manuel@manuelcortez.net>
2023-12-30 17:22:48 -0600

    Updated site URL

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "MCV Software"  # (translatable)
 BLOG_TITLE = "TWBlue"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://twblue.es/"
+SITE_URL = "https://twblue.mcvsoftware.com/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 #BASE_URL = "https://twblue.es"
